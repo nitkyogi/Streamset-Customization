e844b871f4b8277d58e2bde317de960dfb7e1d34
Yogesh Choudhary <yogesh.choudhary@clairvoyantsoft.com>
2016-08-19 12:50:31 +0530

    deleted the data

--- basic-lib/target/apache-rat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-                      if [ "streamsets-datacollector-basic-lib" == "streamsets-datacollector" ];
-                      then
-                      mvn apache-rat:check -N
-                      exit $?
-                      fi
